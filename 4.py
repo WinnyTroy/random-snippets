247f6424f035c9bccc2fdb750e83c74c7a32219e
WinnyTroy <winnie.kiragu@savannahinformatics.com>
2018-05-16 19:49:28 +0300

    Partially attempted question 4

---
+++ 4.py
@@ -0,0 +1,31 @@
+# Order the list
+
+values = [1, 3, -20, -100, 200, 30, 201, -200, 9, 3, 4, 2, -9, 92, 99, -10]
+
+# a.) In ascending Order
+
+values.sort()
+print values
+
+
+# b.) In descending Order
+
+values.reverse()
+print(values)
+
+
+# c.) Get the maximum number in the list
+
+print max(values)
+
+# d.) Get the minimum number in the list
+
+print min(values)
+
+
+# e.) Get the average of the list
+
+average = sum(values)/len(values)
+print average
+
+
