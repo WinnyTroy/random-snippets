f5c5195661e37711918deaa7ca80c89498122b15
WinnyTroy <winnie.kiragu@savannahinformatics.com>
2018-05-16 20:31:02 +0300

    finalised attempts on question 4

--- 4.py
+++ 4.py
@@ -1,31 +1,37 @@
-# Order the list
+# # Order the list
 
 values = [1, 3, -20, -100, 200, 30, 201, -200, 9, 3, 4, 2, -9, 92, 99, -10]
 
-# a.) In ascending Order
+# # a.) In ascending Order
 
-values.sort()
-print values
-
-
-# b.) In descending Order
-
-values.reverse()
-print(values)
+# values.sort()
+# print values
 
 
-# c.) Get the maximum number in the list
+# # b.) In descending Order
 
-print max(values)
-
-# d.) Get the minimum number in the list
-
-print min(values)
+# values.reverse()
+# print(values)
 
 
-# e.) Get the average of the list
+# # c.) Get the maximum number in the list
 
-average = sum(values)/len(values)
-print average
+# print max(values)
+
+# # d.) Get the minimum number in the list
+
+# print min(values)
 
 
+# # e.) Get the average of the list
+
+# average = sum(values)/len(values)
+# print average
+
+# f.) list of dictionaries from the list with the key being the absolute value of an element and the value being the cube of that element
+
+final = {}
+
+
+for x in values:
+    print zip(str(abs(x)), str(x**3))
